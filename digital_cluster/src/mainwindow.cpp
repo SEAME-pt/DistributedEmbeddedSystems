804e3384a6d4b858478288a79cd0ec754eecb01d
matde-je <matidlecosta719@gmail.com>
2025-04-12 17:20:09 +0100

    deploy and gtest

--- digital_cluster/src/mainwindow.cpp
+++ digital_cluster/src/mainwindow.cpp
@@ -47,12 +47,12 @@ MainWindow::~MainWindow()
 //connecting to mqtt via cloud or localhost or to jetracer via network
 void    MainWindow::init_mqtt()
 {
+    client->setHostname("972e24210b544ba49bfb9c1d3164d02b.s1.eu.hivemq.cloud"); //cloud
+    client->setPort(8883);
     QString user = qgetenv("user");
     client->setUsername(user); 
     QString pass = qgetenv("password");
     client->setPassword(pass); 
-    client->setHostname("972e24210b544ba49bfb9c1d3164d02b.s1.eu.hivemq.cloud"); //cloud
-    client->setPort(8883);
     // client->setHostname("10.21.221.67"); //when on the same network
     // client->setPort(1883); //cross compiling
     // client->setHostname("127.0.0.1"); //when cross-compiling with jetracer
