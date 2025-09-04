2f01708609ac58ca5ffdd62511ee0aad6b18be95
matildecosta719@gmail.com <matildecosta719@gmail.com>
2025-09-04 14:49:26 +0100

    speed bug fix

--- digital_cluster/src/speed.cpp
+++ digital_cluster/src/speed.cpp
@@ -71,7 +71,7 @@ void Speed::paint_text(QPainter &painter) {
 
 void Speed::set_current(float n) {
     
-     = n * 3.6;
+    current = n * 3.6;
     float new_target = (current * 270.0f) / max;
     new_target = std::min(new_target, 270.0f);
     if (std::abs(new_target - target_angle) > 1.0f) {  
